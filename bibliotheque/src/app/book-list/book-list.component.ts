aaef81190ce7a5eb6d18048d3733b9a499a9d31d
Kadima Philippe <ph.kadima@gmail.com>
2018-10-25 00:51:16 +0200

    Ajout de la connexion a la base de donnée, gestion des livres, ajout, suppression et visualisation

--- bibliotheque/src/app/book-list/book-list.component.ts
+++ bibliotheque/src/app/book-list/book-list.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Book } from '../models/Book.model';
+import { Subscription } from 'rxjs/Subscription';
+import { BooksService } from '../services/books.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  booksSubscription: Subscription; 
+
+  constructor( private booksService : BooksService,
+  			   private routes : Router) { }
 
   ngOnInit() {
+    this.booksSubscription = this.booksService.booksSubject.subscribe(
+      (books: Book[]) => {
+      	this.books = books;
+      }
+    );
+    this.booksService.getBooks();
+    this.booksService.emitBook();
   }
 
+  onNewBook(){
+  	this.routes.navigate(['/books','new']);
+  }
+
+  onDeleteBook(book: Book){
+  	this.booksService.removeBook(book);
+  }
+
+  onViewBook(id: number){
+  	this.routes.navigate(['/books','view',id]);
+  }
+
+  ngOnDestroy(){
+  	this.booksSubscription.unsubscribe();
+  }
 }
--- bibliotheque/src/app/services/books.service.ts
+++ bibliotheque/src/app/services/books.service.ts
@@ -1,9 +1,66 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../models/Book.model';
+import { Subject } from 'rxjs/Subject';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
+  books: Book[] = [];
+  booksSubject = new Subject<Book[]>();
+
   constructor() { }
+
+  emitBook(){
+  	this.booksSubject.next(this.books);
+  }
+
+  saveBooks(){
+  	firebase.database().ref('/books').set(this.books);
+  }
+
+  getBooks(){
+  	firebase.database().ref('/books')
+  	  .on('value', (data)=>{
+  	  	this.books = data.val() ? data.val() : [];
+  	  	this.emitBook();
+  	  });
+  }
+
+  getSingleBook(id: number){
+  	return new Promise(
+  	  (resolve, reject)=>{
+  	  	firebase.database().ref('/books/'+id).once('value').then(
+  	  	  (data)=>{
+  	  	  	resolve(data.val());
+  	  	  }, 
+  	  	  (error)=>{
+  	  	  	reject(error);
+  	  	  }
+  	  	);
+  	  }
+  	);
+  }
+
+  createNewBook(newBook: Book){
+  	this.books.push(newBook);
+  	console.log(this.books);
+  	this.saveBooks();
+  	this.emitBook();
+  }
+
+  removeBook(book: Book){
+  	const bookIndexToRemove = this.books.findIndex(
+  	  (bookEl) => {
+  	  	if(bookEl === book){
+  	  	  return true;
+  	  	}
+  	  }
+  	);
+  	this.books.splice(bookIndexToRemove, 1);
+  	this.saveBooks();
+  	this.emitBook();
+  }
 }
